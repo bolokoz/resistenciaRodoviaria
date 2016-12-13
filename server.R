eb363a307bee01f4430dcbaccfecbcded9ba0bd7
Yuri Becker <Yuri Becker>
2016-12-13 10:50:28 -0400

    init commmit

---
+++ server.R
@@ -0,0 +1,115 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(plotly)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  # renderPlotly() also understands ggplot2 objects!
+  output$plot <- renderPlotly({
+    rLr = function(x)
+      (input$lC1 + (input$lC2 * input$xL / input$gL) + input$lC3 * x)
+    rVr = function(x)
+      (input$vC1 + (input$vC2 * input$xV / input$gV) + input$vC3 * x)
+    rr = function(x)
+      ((input$vC1 + (input$vC2 * input$xV / input$gV) + input$vC3 * x) * input$nV 
+       + (input$lC1 + (input$lC2 * input$xL / input$gL) + input$lC3 * x) * input$nL)
+    
+    rLa = function(x)
+      (input$lCa * x ^ 2 * input$aL)
+    rVa = function(x)
+      (input$vCa * x ^ 2 * input$aV)
+    ra = function(x)
+      (input$vCa * x ^ 2 * input$aV) + (input$lCa * x ^ 2 * input$aL)
+    
+    rLg = (input$gL * 10 * input$i)
+    rVg = (input$gV * 10 * input$i)
+    rg = rLg + rVg
+    
+    if (input$r != 0) {
+      rLc = (698 * input$gL / input$r)
+      rVc = (698 * input$gV / input$r)
+      rc = rLc + rVc
+      
+    } else{
+      rc = 0
+    }
+    
+    
+    pot = function(x)
+      (input$nL * input$P * 2175 / x)
+    pot2 = function(x)
+      (input$nL * input$P * 1115 / x)
+    
+    
+    vel = seq(input$vmin, input$vmax)
+    po = pot(vel) / 1000
+    rt = rr(vel) + ra(vel) + rc + rg
+    
+    data = data.frame(vel, po, rt)
+    
+    
+    plot_ly(data, x = ~ vel) %>%
+      add_trace(y = ~ po, mode = 'lines') %>%
+      add_trace(y = ~ rt, mode = 'lines')
+    
+  })
+  
+  output$hover <- renderPrint({
+    d <- event_data("plotly_hover")
+    if (is.null(d))
+      "Hover events appear here (unhover to clear)"
+    else
+      d
+  })
+  
+  output$rL <- renderText({
+    
+    if (input$r != 0) {
+      rLc = (698 * input$gL / input$r)
+    } else{
+      rLc = 0
+    }
+    
+    paste("Resistencia de uma locomotiva", 
+          input$lC1 + (input$lC2 * input$xL / input$gL) + rLc + (input$gL * 10 * input$i),
+          " + ",
+          input$lC3 , "*v",
+          " + ",
+          input$lCa , "*v^2")
+  })
+  
+  output$rV <- renderText({
+    
+    if (input$r != 0) {
+      rLc = (698 * input$gL / input$r)
+      rVc = (698 * input$gV / input$r)
+      rc = rLc + rVc
+      
+    } else{
+      rc = 0
+    }
+    
+    rLa = function(x)
+      (input$lCa * x ^ 2 * input$aL * input$nL)
+    
+    vel = seq(input$vmin, input$vmax)
+    paste("x->", rLa(vel))
+  })
+  
+  output$rr <- renderText({
+    rr = function(x)
+      (input$vC1 + (input$vC2 * input$xV / input$gV) + input$vC3 * x) + (input$lC1 + (input$lC2 * input$xL / input$gL) + input$lC3 * x)
+    vel = seq(input$vmin, input$vmax)
+    
+    paste(" rr", rr(vel))
+  })
+  
+}
---
+++ ui.R
@@ -0,0 +1,194 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(plotly)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  # Application title
+  titlePanel("Resistencia Ferroviaria"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(sidebarPanel(
+    tabsetPanel(
+      tabPanel(
+        "Locomotiva",
+        numericInput(
+          "nL",
+          "Numero de locomotivas:",
+          min = 0,
+          max = 50,
+          value = 2
+        ),
+        numericInput(
+          "gL",
+          "Peso [kN]:",
+          min = 0,
+          max = 50,
+          value = 1300
+        ),
+        numericInput(
+          "xL",
+          "Numero de eixos:",
+          min = 1,
+          max = 50,
+          value = 4
+        ),
+        numericInput(
+          "aL",
+          "Area frontal [m2]",
+          min = 0,
+          max = 50,
+          value = 10
+        ),
+        numericInput(
+          "P",
+          "Potencia motor [hp]:",
+          min = 0,
+          max = 90000,
+          value = 3000
+        ),
+        numericInput(
+          "lC1",
+          "c1:",
+          min = 0,
+          max = 90000,
+          value = 0.65
+        ),
+        numericInput(
+          "lC2",
+          "c2:",
+          min = 0,
+          max = 90000,
+          value = 125
+        ),
+        numericInput(
+          "lC3",
+          "c3:",
+          min = 0,
+          max = 90000,
+          value = 0.009
+        ),
+        numericInput(
+          "lCa",
+          "ca:",
+          min = 0,
+          max = 90000,
+          value = 0.009
+        ),
+        numericInput(
+          "f",
+          "Aderencia das rodas:",
+          min = 0,
+          max = 10,
+          value = 0.2
+        )
+      ),
+      tabPanel(
+        "Vagoes",
+        numericInput(
+          "nV",
+          "Numero de vagoes:",
+          min = 0,
+          max = 500,
+          value = 80
+        ),
+        numericInput(
+          "gV",
+          "Peso [kN]:",
+          min = 0,
+          max = 50000,
+          value = 1100
+        ),
+        numericInput(
+          "xV",
+          "Quantidade de eixos:",
+          min = 1,
+          max = 50,
+          value = 4
+        ),
+        numericInput(
+          "aV",
+          "Area frontal [m2]",
+          min = 0,
+          max = 50,
+          value = 8.5
+        ),
+        numericInput(
+          "vC1",
+          "c1:",
+          min = 0,
+          max = 90000,
+          value = 0.65
+        ),
+        numericInput(
+          "vC2",
+          "c2:",
+          min = 0,
+          max = 90000,
+          value = 125
+        ),
+        numericInput(
+          "vC3",
+          "c3:",
+          min = 0,
+          max = 90000,
+          value = 0.009
+        ),
+        numericInput(
+          "vCa",
+          "ca:",
+          min = 0,
+          max = 90000,
+          value = 0.009
+        )
+      ),
+      tabPanel(
+        "Curva",
+        numericInput(
+          "r",
+          "Raio da rampa [m]:",
+          min = 0,
+          max = 50000,
+          value = 0
+        )
+      ),
+      tabPanel(
+        "Rampa",
+        numericInput(
+          "i",
+          "Inclininacao da rampa:",
+          min = 0,
+          max = 50,
+          value = 0
+        )
+      ),
+      tabPanel(
+        "Velocidade",
+        numericInput(
+          "vmax",
+          "Velocidade maxima:",
+          min = 0,
+          max = 9000,
+          value = 120
+        ),
+        numericInput(
+          "vmin",
+          "Velocidade minima:",
+          min = 0,
+          max = 5000,
+          value = 10
+        )
+      )
+    )
+  ),
+  # Show a plot of the generated distribution
+  mainPanel(plotlyOutput("distPlot")))
+))
